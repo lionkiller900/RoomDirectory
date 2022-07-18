c34e29fbada7238629554f8578153fa03d57cb62
lionkiller900 <donalddaniels20@hotmail.com>
2022-07-18 23:09:45 +0100

    added usecases for people

---
+++ RoomDirectory/Domain/UseCases/PeopleUseCase.swift
@@ -0,0 +1,40 @@
+//
+//  PeopleUseCase.swift
+//  RoomDirectory
+//
+//  Created by Admin on 18/07/2022.
+//
+
+import Foundation
+
+protocol PeopleUseCase {
+    func execute() async throws -> [PeopleRecord]
+    func getImage(for url: String) async throws -> Data
+}
+
+final class DefaultPeopleUseCase{
+    private var peoplesRepository: PeoplesRepository
+
+    init(peoplesRepository: PeoplesRepository) {
+        self.peoplesRepository = peoplesRepository
+    }
+}
+
+extension DefaultPeopleUseCase: PeopleUseCase {
+    func getImage(for url: String) async throws -> Data {
+        do {
+            return  try await peoplesRepository.getImages(for: url)
+        }catch {
+            throw error
+        }
+    }
+    
+    func execute() async throws -> [PeopleRecord] {
+        do {
+            return try await peoplesRepository.getPeoples()
+        }catch {
+          throw error
+        }
+    }
+}
+
