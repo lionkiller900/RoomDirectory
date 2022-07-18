33bd11d9c093a2c55704b4fc74eb8f20e2575635
lionkiller900 <donalddaniels20@hotmail.com>
2022-07-18 22:48:50 +0100

    added viewmodel for the people

---
+++ RoomDirectory/Presentation/People/PeopleViewModel.swift
@@ -0,0 +1,88 @@
+//
+//  PeopleViewModel.swift
+//  RoomDirectory
+//
+//  Created by Admin on 18/07/2022.
+//
+
+import Foundation
+import Combine
+
+protocol PeopleViewModelAction {
+    func navigateToDetails(for index:Int)
+}
+
+protocol PeopleViewModelInput {
+    func getPeoples() async
+    func getPeopleImage(for index: Int, completion:@escaping (Data) -> Void)
+}
+
+protocol PeopleViewModelOutput {
+    var state: PeopleViewStates {get}
+    var numberOfPeople:Int {get}
+    func getPeople(for index: Int)-> PeopleRecord
+}
+
+final class PeopleViewModel {
+    
+    private var peopleUseCase: PeopleUseCase
+
+    private var peopleRecords: [PeopleRecord] = []
+ 
+    private weak var coordinator: PeoplesCoordinator?
+
+    @Published  var state: PeopleViewStates = .none
+    
+    private var cancellables:Set<AnyCancellable> = Set()
+    
+    init(peopleUseCase: PeopleUseCase,
+         coordinator: PeoplesCoordinator) {
+        self.peopleUseCase = peopleUseCase
+        self.coordinator = coordinator
+    }
+}
+
+extension PeopleViewModel: PeopleViewModelAction {
+    func navigateToDetails(for index: Int) {
+        
+    }
+}
+
+extension PeopleViewModel: PeopleViewModelOutput {
+    var numberOfPeople:Int {
+        return peopleRecords.count
+    }
+    
+    func getPeople(for index: Int)-> PeopleRecord {
+        return peopleRecords[index]
+    }
+}
+
+extension PeopleViewModel: PeopleViewModelInput {
+    
+    func getPeopleImage(for index: Int, completion:@escaping (Data) -> Void)  {
+        Task {
+            do {
+                let imageData =  try await peopleUseCase.getImage(for:peopleRecords[index].avatar)
+
+                DispatchQueue.main.async {
+                    completion(imageData)
+                }
+            }catch {
+                
+            }
+        }
+    }
+    
+    func getPeoples() async {
+        state = .showActivityIndicator
+        do {
+            peopleRecords = try await  peopleUseCase.execute()
+            
+            state = .showPeopleView
+        } catch {
+            state = .showError((error as! APIError).localizedDescription)
+        }
+    }
+}
+
