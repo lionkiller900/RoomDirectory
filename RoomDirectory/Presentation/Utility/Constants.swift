e89a8863e18f8126225998faf7c2bede68f4233c
lionkiller900 <donalddaniels20@hotmail.com>
2022-07-18 22:55:50 +0100

    created utility file for constants

---
+++ RoomDirectory/Presentation/Utility/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  RoomDirectory
+//
+//  Created by Admin on 18/07/2022.
+//
+
+import Foundation
+
+struct Constants {
+   
+    // Column Size
+    static let  iPhoneColumnCount = 3
+    static let  iPadColumnCount = 4
+}
+
