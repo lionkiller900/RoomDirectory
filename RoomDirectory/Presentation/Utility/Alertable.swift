3bdfebcb8d36c5f574f198188161fdd799ad3e8a
lionkiller900 <donalddaniels20@hotmail.com>
2022-07-18 22:58:33 +0100

    created alertable file under utility

---
+++ RoomDirectory/Presentation/Utility/Alertable.swift
@@ -0,0 +1,18 @@
+//
+//  Alertable.swift
+//  RoomDirectory
+//
+//  Created by Admin on 18/07/2022.
+//
+
+import UIKit
+
+public protocol Alertable {}
+public extension Alertable where Self: UIViewController {
+    
+    func showAlert(title: String = NSLocalizedString("alert", comment:""), message: String, preferredStyle: UIAlertController.Style = .alert, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: completion)
+    }
+}
