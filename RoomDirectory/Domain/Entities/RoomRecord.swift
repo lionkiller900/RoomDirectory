7765b373679bc7e2ef6a77f388862cd8d16f5b10
lionkiller900 <donalddaniels20@hotmail.com>
2022-07-18 23:13:59 +0100

    added entities for room record

---
+++ RoomDirectory/Domain/Entities/RoomRecord.swift
@@ -0,0 +1,16 @@
+//
+//  RoomRecord.swift
+//  RoomDirectory
+//
+//  Created by Admin on 18/07/2022.
+//
+
+import Foundation
+
+struct RoomRecord {
+    var createdAt: String
+    var occupiedMessage: String
+    var maxOccupancy: Int
+    var id: String
+}
+
