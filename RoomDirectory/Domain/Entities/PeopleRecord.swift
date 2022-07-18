26eb85a31094f4b3ab134cceaf1fc65f66ae5dfc
lionkiller900 <donalddaniels20@hotmail.com>
2022-07-18 23:11:45 +0100

    added people's record for entities

---
+++ RoomDirectory/Domain/Entities/PeopleRecord.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleRecord.swift
+//  RoomDirectory
+//
+//  Created by Admin on 18/07/2022.
+//
+
+import Foundation
+
+struct PeopleRecord: Equatable {
+    var firstName: String
+    var avatar: String
+    var lastName: String
+    var email: String
+    var jobTitle: String
+    var joinedDate: String
+}
+
